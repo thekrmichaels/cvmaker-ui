4f2073fdb8c4472aba435a9265427b1cf23b3dab
Amilkar Díaz <amilkarjdiazh@outlook.com>
2025-01-13 12:09:54 -0500

    build: 🛂 install and use Auth0

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0 } from "../env.js";
 import { PrimeReactProvider } from "primereact/api";
 import "./index.css";
 import App from "./App.jsx";
 
+const { clientId, domain } = Auth0;
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <PrimeReactProvider>
-      <App />
-    </PrimeReactProvider>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <PrimeReactProvider>
+        <App />
+      </PrimeReactProvider>
+    </Auth0Provider>
   </StrictMode>,
 );
