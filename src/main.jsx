4e3dbfc85f8df75655d5994e77933c972b8ae2b4
Amilkar Díaz <amilkarjdiazh@outlook.com>
2025-02-03 16:48:35 -0500

    ci: 👷‍♂️ use GitHub Actions and Pages
    Configure the repository to deploy.

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")).render(
       domain={domain}
       clientId={clientId}
       authorizationParams={{
-        redirect_uri: window.location.origin,
+        redirect_uri: window.location.origin + "/cvmaker",
       }}
     >
       <PrimeReactProvider>
