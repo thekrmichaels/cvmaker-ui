3a9c972966f3f52b9ee20e2a1051c91cb31dad75
Amilkar Díaz <amilkarjdiazh@outlook.com>
2025-02-11 09:54:12 -0500

    build: ➕ use vite-plugin-html

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          version: Date.now(),
+        },
+      },
+    }),
+  ],
   base: "/cvmaker",
 });
