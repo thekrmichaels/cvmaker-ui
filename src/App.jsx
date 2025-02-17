c4b7d8be98cf518c588799120baddd66128e9f41
Amilkar Díaz <amilkarjdiazh@outlook.com>
2025-02-16 20:48:05 -0500

    feat: 💄 main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/prop-types */
+import { Link } from "react-router";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useAuthContext, useThemeContext } from "./hooks/useContexts";
+import GoogleSignInButton from "./components/Google/GoogleSignInButton";
+import UserDropdown from "./components/UserDropdown";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App({ children }) {
+  const { isAuthenticated, isLoading } = useAuth0();
+  const { user } = useAuthContext();
+  const { themeStyle } = useThemeContext();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      className={`relative flex h-auto min-h-screen justify-center ${themeStyle}`}
+    >
+      <nav className="absolute top-0 flex w-full items-center justify-between px-4 py-2">
+        <div className="flex-1">
+          <Link to="/">Home</Link>
+        </div>
+        <div className="flex flex-1 justify-center">
+          <Link to="/pricing">Pricing</Link>
+        </div>
+        <div className="flex-1 text-right">
+          {isAuthenticated ? (
+            <UserDropdown user={user} />
+          ) : (
+            <GoogleSignInButton />
+          )}
+        </div>
+      </nav>
+      {isLoading ? <Loading themeStyle={themeStyle} /> : children}
+    </div>
+  );
 }
 
-export default App
+const Loading = ({ themeStyle }) => {
+  return (
+    <div
+      className={`absolute inset-0 flex items-center justify-center bg-opacity-50 ${themeStyle}`}
+    >
+      <p className="animate-pulse">Loading</p>
+    </div>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { Auth0 } from "../env.js";
 import { AuthProvider } from "./contexts/AuthProvider.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 import { PrimeReactProvider } from "primereact/api";
 import { ThemeProvider } from "./contexts/ThemeProvider.jsx";
 import "./index.css";
 import App from "./App.jsx";
+import Home from "./routes/Home.jsx";
+import Pricing from "./routes/Pricing.jsx";
 
 const { clientId, domain } = Auth0;
 
@@ -20,11 +23,18 @@ createRoot(document.getElementById("root")).render(
       }}
     >
       <AuthProvider>
-        <PrimeReactProvider>
-          <ThemeProvider>
-            <App />
-          </ThemeProvider>
-        </PrimeReactProvider>
+        <BrowserRouter basename="/cvmaker">
+          <PrimeReactProvider>
+            <ThemeProvider>
+              <App>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/pricing" element={<Pricing />} />
+                </Routes>
+              </App>
+            </ThemeProvider>
+          </PrimeReactProvider>
+        </BrowserRouter>
       </AuthProvider>
     </Auth0Provider>
   </StrictMode>,
